359092e475bac37a6f22549769053fc875cccfa3
Vsevolod <kefirchik3@gmail.com>
2018-11-28 02:42:36 +0200

    Half complete java_lab3

---
+++ 2018-2019/oop/java_lab3/src/Main.java
@@ -0,0 +1,68 @@
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.Arrays;
+import java.util.function.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Виконав:\tГрупа:\nАмброс В. В.\tІП-71\n" +
+            "--------------------\n");
+
+        RecBook[] recbooks = new RecBook[10];
+        for (int i = 0; i < 10; ++i) {
+            recbooks[i] = createRandomRecBook();
+//            System.out.println(recbooks[i].toString());
+//            System.out.println("------------------------------");
+        }
+
+        RecBook[] filtered = Arrays.stream(recbooks)
+            .filter(recBook -> recBook.getAvgScore() > 4)
+            .toArray(RecBook[]::new);
+
+        for (RecBook rb : filtered) {
+            System.out.println(rb.toString());
+        }
+
+    }
+
+    private static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    private static RecBook createRandomRecBook() {
+        Subject[] subjects = new Subject[5];
+        for (int i = 0; i < 5; ++i) {
+            subjects[i] = createRandomSubject();
+        }
+
+        return new RecBook(
+            randString(true),
+            randString(true),
+            randString(true),
+            (short) randRange(1, 5),
+            subjects
+        );
+    }
+
+    private static Subject createRandomSubject() {
+        return new Subject(
+            randString(false),
+            randRange(2, 6)
+        );
+    }
+
+    private static int randRange(int from, int to) {
+        return (int) (Math.random() * (to - from)) + from;
+    }
+
+    private static String randString(boolean doCapitalize) {
+        int length = randRange(5, 11);
+        StringBuilder res = new StringBuilder();
+        for (var i = 0; i < length; ++i) {
+            res.append(
+                alphabet.charAt(randRange(0, alphabet.length()))
+            );
+        }
+        if (doCapitalize)
+            res.setCharAt(0, Character.toUpperCase(res.charAt(0)));
+
+        return res.toString();
+    }
+}
---
+++ 2018-2019/oop/java_lab3/src/RecBook.java
@@ -0,0 +1,64 @@
+public class RecBook {
+    private String name, surname, patronymic;
+    private short course;
+    private Subject[] subjects;
+
+    public RecBook() {
+    }
+
+    public RecBook(
+        String _name,
+        String _surname,
+        String _patronymic,
+        short _course,
+        Subject[] _subjects
+    ) {
+        name = _name;
+        surname = _surname;
+        patronymic = _patronymic;
+        course = _course;
+        subjects = new Subject[_subjects.length];
+        System.arraycopy(_subjects, 0,
+            subjects, 0, _subjects.length);
+    }
+
+    public String toString() {
+        StringBuilder subjectsStr = new StringBuilder();
+        for (var subject : subjects) {
+            subjectsStr.append(String.format(
+                "Предмет:\t%s\nБал:\t\t%s",
+                subject.name,
+                subject.score
+            )).append("\n---------------\n");
+        }
+
+        return String.format(
+            "Прізвище:\tІм'я:\tПо-батькові:\n%s\t%s\t%s\n" +
+                "Курс: %s\n" +
+                "Середній бал: %s\n",
+            name,
+            surname,
+            patronymic,
+            course,
+            getAvgScore()
+        ) + "--------------------\n" + subjectsStr;
+    }
+
+    public float getAvgScore() {
+        float res = 0;
+        for (var subject : subjects) {
+            res += subject.score;
+        }
+        return res / subjects.length;
+    }
+}
+
+class Subject {
+    String name;
+    float score;
+
+    public Subject(String _name, float _score) {
+        name = _name;
+        score = _score;
+    }
+}
